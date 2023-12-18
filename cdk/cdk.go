fdad47a8c4fc76d285532b1c7410e6cd62501c44
Walter Scott <w@lter.ca>
2023-12-18 15:24:25 -0600

    more vendor

--- cdk/cdk.go
+++ cdk/cdk.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -21,9 +21,9 @@ func NewCdkStack(scope constructs.Construct, id string, props *CdkStackProps) aw
 	// The code that defines your stack goes here
 
 	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("CdkQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	_ = awssqs.NewQueue(stack, jsii.String("CdkQueue"), &awssqs.QueueProps{
+		VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
+	})
 
 	return stack
 }
